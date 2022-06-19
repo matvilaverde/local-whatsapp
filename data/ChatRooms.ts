2b0116d7b1949a237ef50f3d6985508ce085f32a
Mateus Vilaverde <mateusvilaverde@gmail.com>
2022-06-19 18:10:01 -0300

    Joking around with portuguese messages

--- data/ChatRooms.ts
+++ data/ChatRooms.ts
@@ -12,7 +12,39 @@ export default [{
     lastMessage: {
         id: 'm1',
         content: 'Coração das cartas, ridículo',
-        createdAt: '2020-10-03T14:48:00.000Z',
+        createdAt: '2022-06-19T18:07:00.000Z',
+    },
+}, {
+    id: '2',
+    users: [{
+        id: 'u1',
+        name: 'Seto Kaiba',
+        imageUri: 'https://th.bing.com/th/id/OIP.jOvkMry6GB0S5CGwCX8_0QHaEK?pid=ImgDet&rs=1',
+    }, {
+        id: 'u2',
+        name: 'Yami',
+        imageUri: 'https://i.pinimg.com/originals/2d/7c/1d/2d7c1dff6b99a20aa5e2f467e0acb81b.jpg',
+    }],
+    lastMessage: {
+        id: 'm2',
+        content: 'Abra sua mente!!!',
+        createdAt: '2022-06-19T18:08:00.000Z',
+    },
+}, {
+    id: '3',
+    users: [{
+        id: 'u1',
+        name: 'Yami',
+        imageUri: 'https://i.pinimg.com/originals/2d/7c/1d/2d7c1dff6b99a20aa5e2f467e0acb81b.jpg',
+    }, {
+        id: 'u3',
+        name: 'Maximillion Pegasus',
+        imageUri: 'https://th.bing.com/th/id/OIP.wK-xMMlnCdmdcTPYnsmk5wHaHR?w=179&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    }],
+    lastMessage: {
+        id: 'm3',
+        content: 'Uhuhu do mundo da fantasia',
+        createdAt: '2022-06-19T18:09:00.000Z',
     },
 }]
 // [{
