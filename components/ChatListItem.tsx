f5c5295e8e242b13ab57dcecef108b11097c41a3
Mateus Vilaverde <mateusvilaverde@gmail.com>
2022-06-19 20:01:21 -0300

    Formating date without moment

--- components/ChatListItem.tsx
+++ components/ChatListItem.tsx
@@ -20,17 +20,43 @@ const ChatListItem = (props: ChatListItemProps) => {
 				<View style={styles.midContainer}>
 					<View style={styles.topSection}>
 						<Text numberOfLines={1} style={styles.username}>{user.name}</Text>
-						<Text style={styles.time}>Yesterday</Text>
+						<Text style={styles.time}>
+							{dateFormatter('DMY', chatRoom.lastMessage.createdAt)}
+						</Text>
 					</View>
 					<Text numberOfLines={1} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
 				</View>
 			
 			</View>
-
-			{/* <Text>{chatRoom.lastMessage.createdAt}</Text> */}
-			
 		</View>
 	)
 };
+
+/** Formats date according to format given and ignores depreciated Moment */
+const dateFormatter = (format: string, unformatedDate: string) => {
+	let formatedDate: string;
+	const day = new Date(unformatedDate).getDate();
+	const month = new Date(unformatedDate).getMonth() + 1;
+	const year = new Date(unformatedDate).getFullYear();
+
+	switch (format) {
+		case 'DMY':
+			formatedDate = (day < 10 ? '0' : '') + day + '/' + (month < 10 ? '0' : '') + month
+				+ '/' + year;
+			break;
+			
+		case 'MDY':
+			formatedDate = (month < 10 ? '0' : '') + month + '/' + (day < 10 ? '0' : '') + day
+				+ '/' + year;
+			break;
+			
+		default:
+			formatedDate = year + '/' + (month < 10 ? '0' : '') + month + '/' + (day < 10 ?
+				'0' : '') + day;
+			break;
+	}
+	
+	return formatedDate;
+}
 
 export default ChatListItem;
